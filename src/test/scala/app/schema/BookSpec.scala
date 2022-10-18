4440a8499c358f8578b7d2b50f737f6c028ac992
Ethan Zhang <ethan6077@gmail.com>
2022-10-19 08:29:41 +1100

    decode spec done

--- src/test/scala/app/schema/BookSpec.scala
+++ src/test/scala/app/schema/BookSpec.scala
@@ -3,6 +3,7 @@ package app.schema
 import java.time.LocalDate
 import io.circe.literal.JsonStringContext
 import io.circe.syntax.EncoderOps
+import io.circe.parser.decode
 import org.specs2.mutable.Specification
 
 class BookSpec extends Specification {
@@ -20,6 +21,24 @@ class BookSpec extends Specification {
       val releaseDate = LocalDate.parse("2022-10-01")
       val myBook = Book("ethan", "Magic Book", Comic, 100, releaseDate)
       myBook.asJson should beEqualTo(expectedJson)
+    }
+  }
+
+  "BookDecoder" should {
+    "decode stringified json to Book" in {
+      val rawJson =
+        json"""{
+          "author": "ethan",
+          "title": "Magic Book",
+          "type": "COMIC",
+          "pages": 100,
+          "releaseDate": "2022-10-01"
+        }"""
+
+      val releaseDate = LocalDate.parse("2022-10-01")
+      decode[Book](rawJson.noSpaces) should beRight(
+        Book("ethan", "Magic Book", Comic, 100, releaseDate)
+      )
     }
   }
 }
